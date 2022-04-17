e203728fd5af86db066fdb5cacc22ba12b836b72
crucker3 <98133127+crucker3@users.noreply.github.com>
2022-04-17 00:24:35 -0500

    Add files via upload

---
+++ businesses.js
@@ -0,0 +1,32 @@
+var lnkbldngsjson ={
+"type": "FeatureCollection",
+"name": "businesses",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Buiness type": "daycare", "Lat": 40.85345034, "Long": -96.62565423 }, "geometry": { "type": "Point", "coordinates": [ -96.62565423, 40.85345034 ] } },
+{ "type": "Feature", "properties": { "Buiness type": "meeting place", "Lat": 40.85221683, "Long": -96.62677003 }, "geometry": { "type": "Point", "coordinates": [ -96.62677003, 40.85221683 ] } },
+{ "type": "Feature", "properties": { "Buiness type": "electric repair", "Lat": 40.85284981, "Long": -96.62707044 }, "geometry": { "type": "Point", "coordinates": [ -96.62707044, 40.85284981 ] } },
+{ "type": "Feature", "properties": { "Buiness type": "auto repair", "Lat": 40.85134037, "Long": -96.62516071 }, "geometry": { "type": "Point", "coordinates": [ -96.62516071, 40.85134037 ] } },
+{ "type": "Feature", "properties": { "Buiness type": "auto sales", "Lat": 40.85057752, "Long": -96.62522508000001 }, "geometry": { "type": "Point", "coordinates": [ -96.62522508, 40.85057752 ] } },
+{ "type": "Feature", "properties": { "Buiness type": "contracting", "Lat": 40.85005812, "Long": -96.62593318 }, "geometry": { "type": "Point", "coordinates": [ -96.62593318, 40.85005812 ] } },
+{ "type": "Feature", "properties": { "Buiness type": "museum", "Lat": 40.84981466, "Long": -96.62114812 }, "geometry": { "type": "Point", "coordinates": [ -96.62114812, 40.84981466 ] } },
+{ "type": "Feature", "properties": { "Buiness type": "medical billing", "Lat": 40.84934395, "Long": -96.62018253 }, "geometry": { "type": "Point", "coordinates": [ -96.62018253, 40.84934395 ] } },
+{ "type": "Feature", "properties": { "Buiness type": "applinace repair", "Lat": 40.84773704, "Long": -96.62239267 }, "geometry": { "type": "Point", "coordinates": [ -96.62239267, 40.84773704 ] } },
+{ "type": "Feature", "properties": { "Buiness type": "interior design", "Lat": 40.84650342, "Long": -96.62099792 }, "geometry": { "type": "Point", "coordinates": [ -96.62099792, 40.84650342 ] } },
+{ "type": "Feature", "properties": { "Buiness type": "coop of rail roaders", "Lat": 40.85182729, "Long": -96.62846519 }, "geometry": { "type": "Point", "coordinates": [ -96.62846519, 40.85182729 ] } },
+{ "type": "Feature", "properties": { "Buiness type": "construction", "Lat": 40.8525739, "Long": -96.62885142 }, "geometry": { "type": "Point", "coordinates": [ -96.62885142, 40.8525739 ] } },
+{ "type": "Feature", "properties": { "Buiness type": "public pool", "Lat": 40.85280112, "Long": -96.63061095 }, "geometry": { "type": "Point", "coordinates": [ -96.63061095, 40.85280112 ] } },
+{ "type": "Feature", "properties": { "Buiness type": "church", "Lat": 40.8511456, "Long": -96.6307397 }, "geometry": { "type": "Point", "coordinates": [ -96.6307397, 40.8511456 ] } },
+{ "type": "Feature", "properties": { "Buiness type": "plastics manufacturer", "Lat": 40.85187598, "Long": -96.63331462 }, "geometry": { "type": "Point", "coordinates": [ -96.63331462, 40.85187598 ] } },
+{ "type": "Feature", "properties": { "Buiness type": "towing", "Lat": 40.85005812, "Long": -96.63370086 }, "geometry": { "type": "Point", "coordinates": [ -96.63370086, 40.85005812 ] } },
+{ "type": "Feature", "properties": { "Buiness type": "drywall", "Lat": 40.84910048, "Long": -96.63258506 }, "geometry": { "type": "Point", "coordinates": [ -96.63258506, 40.84910048 ] } },
+{ "type": "Feature", "properties": { "Buiness type": "school", "Lat": 40.84616255, "Long": -96.6331215 }, "geometry": { "type": "Point", "coordinates": [ -96.6331215, 40.84616255 ] } },
+{ "type": "Feature", "properties": { "Buiness type": "tree removal", "Lat": 40.84590283, "Long": -96.63513852 }, "geometry": { "type": "Point", "coordinates": [ -96.63513852, 40.84590283 ] } },
+{ "type": "Feature", "properties": { "Buiness type": "appliance shop", "Lat": 40.85481157, "Long": -96.62375783 }, "geometry": { "type": "Point", "coordinates": [ -96.62375783, 40.85481157 ] } },
+{ "type": "Feature", "properties": { "Buiness type": "church", "Lat": 40.85546126, "Long": -96.63648462 }, "geometry": { "type": "Point", "coordinates": [ -96.63648462, 40.85546126 ] } },
+{ "type": "Feature", "properties": { "Buiness type": "equipment repair", "Lat": 40.85127128, "Long": -96.63899141 }, "geometry": { "type": "Point", "coordinates": [ -96.63899141, 40.85127128 ] } },
+{ "type": "Feature", "properties": { "Buiness type": "auto repair", "Lat": 40.85119119, "Long": -96.63793526000001 }, "geometry": { "type": "Point", "coordinates": [ -96.63793526, 40.85119119 ] } },
+{ "type": "Feature", "properties": { "Buiness type": "gas station", "Lat": 40.84924346, "Long": -96.63956604000001 }, "geometry": { "type": "Point", "coordinates": [ -96.63956604, 40.84924346 ] } },
+{ "type": "Feature", "properties": { "Buiness type": "car wash", "Lat": 40.84857798, "Long": -96.63956604000001 }, "geometry": { "type": "Point", "coordinates": [ -96.63956604, 40.84857798 ] } }
+]
+}
