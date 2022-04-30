220440fb9b8f187861e4de4f87998a3450efd474
crucker3 <98133127+crucker3@users.noreply.github.com>
2022-04-30 15:04:53 -0500

    Add files via upload

---
+++ chicken.js
@@ -0,0 +1,14 @@
+var chicken = {
+"type": "FeatureCollection",
+"name": "RestaurantLocations",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "KFC", "Long": 40.8349512, "Lat": -96.60433403, "type": "Fried Chicken", "Level": "National" }, "geometry": { "type": "Point", "coordinates": [ -96.60433403, 40.8349512 ] } },
+{ "type": "Feature", "properties": { "name": "KFC", "Long": 40.83858769, "Lat": -96.70527091, "type": "Fried Chicken", "Level": "National" }, "geometry": { "type": "Point", "coordinates": [ -96.70527091, 40.83858769 ] } },
+{ "type": "Feature", "properties": { "name": "KFC", "Long": 40.79154206, "Lat": -96.70803595, "type": "Fried Chicken", "Level": "National" }, "geometry": { "type": "Point", "coordinates": [ -96.70803595, 40.79154206 ] } },
+{ "type": "Feature", "properties": { "name": "Popeyes", "Long": 40.85407848, "Lat": -96.68213533, "type": "Fried Chicken", "Level": "National" }, "geometry": { "type": "Point", "coordinates": [ -96.68213533, 40.85407848 ] } },
+{ "type": "Feature", "properties": { "name": "Popeyes", "Long": 40.81407542, "Lat": -96.73123048, "type": "Fried Chicken", "Level": "National" }, "geometry": { "type": "Point", "coordinates": [ -96.73123048, 40.81407542 ] } },
+{ "type": "Feature", "properties": { "name": "Popeyes", "Long": 40.82057106, "Lat": -96.65398287, "type": "Fried Chicken", "Level": "National" }, "geometry": { "type": "Point", "coordinates": [ -96.65398287, 40.82057106 ] } },
+{ "type": "Feature", "properties": { "name": "Popeyes", "Long": 40.76884811, "Lat": -96.62514376, "type": "Fried Chicken", "Level": "National" }, "geometry": { "type": "Point", "coordinates": [ -96.62514376, 40.76884811 ] } },
+]
+}
